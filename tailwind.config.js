7bc9434170b69ec90ae9bd3285afe07fb90b0931
Armendd21 <ademiarmend50@gmail.com>
2024-05-06 00:40:10 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'brand-yellow':'#b5911b',
+        'brand-blue':'#478eed',
+        'brand-border-gray':'#e2e2e2',
+        'brand-gray':'#1f2120',
+        'brand-red':'#f89b29',
+        'brand-main':'#b5c6e0',
+        'brand-gaming':'#000328',
+        'brand-h1-darkblue':'#000015',
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
